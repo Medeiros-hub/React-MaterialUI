1bfa637a8c66d3d03dec8ecc069653fd4e13f420
Pedro Medeiros <pmedeirosramalho@gmail.com>
2022-12-18 20:56:37 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './Routes';
+import { LightTheme } from './shared/themes';
+
+
+export const App = () => {
+  return (
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
---
+++ src/Routes/index.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate} from 'react-router-dom'
+
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path='/home' element={
+                <Button variant='contained' color='primary'>Teste</Button>}
+            ></Route>
+
+            <Route path='*' element={
+                <Navigate to="/home"/>
+            }></Route>
+        </Routes>
+    );
+}
